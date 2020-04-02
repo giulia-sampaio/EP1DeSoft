c3a4a3b25783b838e473de8823f1695d409d44f4
Giulia Sampaio <giuliacms@al.insper.edu.br>
2020-04-02 18:44:16 -0300

    Versão final do projeto

--- EP1DeSoft_GiuliaSampaio.py
+++ EP1DeSoft_GiuliaSampaio.py
@@ -1,16 +1,16 @@
+# Importando a biblioteca ramdom:
 import random 
 
-jogando = True
-
+# Declarando a quantidade de fichas inicial do usuário:
 fichas = 500
+
+# Iniciando o jogo:
+jogando = True
 
 while jogando == True:
 
     pass_line_bet = False
     point = False
-    field = False
-    any_craps = False
-    twelve = False
 
     resp = input("Apostar ou sair do jogo? ")
 
@@ -31,15 +31,16 @@ while jogando == True:
             print("Suas fichas não são suficientes para essa aposta!")
             break
 
-        print("As apostas que você pode fazer são: Pass Line Bet, Field, Any Craps ou Twelve")
-        print("Observação: digite o nome da aposta com letras minusculas")
-        aposta = input("Qual modalidade de aposta você quer? ")
+        print("As apostas que você pode fazer são: Pass Line Bet(1), Field(2), Any Craps(3) ou Twelve(4)")
+        aposta = int(input("Qual modalidade de aposta você quer? "))
      
-        if aposta == "pass line bet":
+        # Iniciando aposta Pass Line Bet
+        if aposta == 1:
             pass_line_bet = True
 
         while pass_line_bet == True:
 
+            # Jogando os dados:
             dado = random.randint(2,12)
             print("Você tirou {0}".format(dado))
 
@@ -47,20 +48,20 @@ while jogando == True:
                 fichas = fichas - valor
                 print("Você perdeu! Agora você tem {0} fichas =(".format(fichas))
                 pass_line_bet = False
-                point = False
 
-            if dado == 7 or dado == 11:
+            elif dado == 7 or dado == 11:
                 fichas = fichas + valor
                 print("Você ganhou! Agora você tem {0} fichas =)".format(fichas))
                 pass_line_bet = False
-                point = False
 
+            # Iniciando fase Point:
             else:
                 point = True
                 print("Você passou para a fase Point!")
 
                 while point == True:
 
+                    # Jogando um segundo dado:
                     dado2 = random.randint(2,12)
                     print("Você tirou {0}".format(dado2))
 
@@ -79,66 +80,54 @@ while jogando == True:
                     else:
                         print("Os dados serão jogados novamente...")
 
-        if aposta == "field":
-            field = True
+        # Iniciando aposta Field:
+        if aposta == 2:
 
-        while field == True:
-
+            # Jogando os dados:
             dado = random.randint(2,12)
             print("Você tirou {0}".format(dado))
 
             if dado == 5 or dado == 6 or dado == 7 or dado == 8:
                 fichas = fichas - valor
                 print("Você perdeu! Agora você tem {0} fichas =(".format(fichas))
-                field = False
 
-            if dado == 3 or dado == 4 or dado == 9 or dado == 10 or dado == 11:
+            elif dado == 3 or dado == 4 or dado == 9 or dado == 10 or dado == 11:
                 fichas = fichas + valor
                 print("Você ganhou! Agora você tem {0} =)".format(fichas))
-                field = False
 
             elif dado == 2:
                 fichas = fichas + 2*valor
                 print("Você ganhou! Agora você tem {0} =)".format(fichas))
-                field = False
 
             else:
                 fichas = valor + 3*valor
-                print("Você ganhou! Agora você tem {0} =)".format(fichas))
-                field = False 
 
-        if aposta == "any craps":
-            any_craps = True
-               
-        while any_craps == True:
+        # Iniciando aposta Any Craps:
+        if aposta == 3:
 
+            # Jogando os dados:
             dado = random.randint(2,12)
             print("Você tirou {0}".format(dado))
 
             if dado == 2 or dado == 3 or dado == 12:
                 fichas = fichas + 7*valor
                 print("Você ganhou! Agora você tem {0} =)".format(fichas))
-                any_craps = False
 
             else:
                 fichas = fichas - valor
                 print("Você perdeu! Agora você tem {0} fichas =(".format(fichas))
-                any_craps = False 
 
-        if aposta == "twelve":
-            twelve = True 
+        # Iniciando aposta Twelve:
+        if aposta == 4:
 
-        while twelve == True:
-
+            # Jogando os dados:
             dado = random.randint(2,12)
             print("Você tirou {0}".format(dado))
 
             if dado == 12:
                 fichas = fichas + 30*valor
                 print("Você ganhou! Agora você tem {0} =)".format(fichas))
-                twelve = False
 
             else:
                 fichas = fichas - valor
                 print("Você perdeu! Agora você tem {0} fichas =(".format(fichas))
-                twelve = False
